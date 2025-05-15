1e98ce83ca4b9a509f048870136c7a539ad3ee5f
Sadiya-Mehreen <sadiyamehreenaiml@gmail.com>
2025-05-16 01:29:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,14 @@ import gdown
 
 st.title("🔥 Fire Detection App (Test Deployment)")
 
-# Model file name
 model_path = "best.pt"
 
-# Download model if not exists
 if not os.path.exists(model_path):
     st.info("Downloading model file...")
-    # Replace this with your Google Drive file ID for best.pt
-    url = "https://drive.google.com/file/d/1INHMFa1yb3reB6ezi7nJvjGOVEyvvIAm/view?usp=drive_link"
+    url = "https://drive.google.com/uc?id=1INHMFa1yb3reB6ezi7nJvjGOVEyvvIAm"
     gdown.download(url, model_path, quiet=False)
     st.success("Model downloaded!")
 
-# Placeholder for YOLO import and prediction
 try:
     from ultralytics import YOLO
 
@@ -26,4 +22,3 @@ try:
 
 except Exception as e:
     st.error(f"Error loading YOLO model: {e}")
-
